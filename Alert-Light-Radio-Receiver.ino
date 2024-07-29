7a7fcd8d5fb32f1534d59c2698362e26943e51c5
Samuel Handy <Samuel.Handy3@gmail.com>
2024-07-29 18:45:51 -0400

    Final fucntionality added, code cleaned up, tested, and organized

--- Alert-Light-Radio-Receiver.ino
+++ Alert-Light-Radio-Receiver.ino
@@ -1,12 +1,16 @@
+/*******************INCLUDES***********************/
+
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
 
+/*******************VARIABLES**********************/
+
 //Unique Pipe address
 const uint64_t pipeInAddress = 0xE8E8F0F0E1LL; 
 
-//CSN and CE pins of the NRF. Watch video for exact wire positions
-RF24 radio(9, 10);  //CSN and CE pins if the NRF
+//CSN and CE pins of the NRF
+RF24 radio(9, 10); 
 
 //0 = OFF
 //1 = ON
@@ -24,11 +28,11 @@ int BUTTON_DIGITAL_INPUT_PIN = 4;
 //The ON/OFF state of the button
 int BUTTON_STATE = 0;
 
-/**************************************************/
+/*********************SETUP************************/
 
 void setup()
 {
-  //Setup LED Pin to Output
+  //Setup Pins to Output
   pinMode(BUTTON_LED_POWER_PIN,OUTPUT);
   pinMode(BUTTON_DIGITAL_INPUT_PIN,INPUT_PULLUP);
   //Setup Radio
@@ -39,49 +43,37 @@ void setup()
   radio.startListening();
 }
 
-/**************************************************/
+/*******************resetData()********************/
 
 void resetData() 
 {
   received_data.light_status = 0;  
 }
 
-/**************************************************/
+/***************receive_the_data()*****************/
 
-
-/******Reset the received data to 0 if connection is lost******/
-unsigned long last_Time = 0;
-
-
-//We create the function that will read the data each certain time
 void receive_the_data()
 {
   while ( radio.available() && received_data.light_status == 0) 
   {
-    //In the provided code, &received_data is a pointer to a variable of type Received_data, which is a struct containing a single byte of data. 
-    //sizeof(Received_data) is the size of the Received_data struct in bytes, which is one byte.
+    //Reads the data and sets it to the received_data struct
     radio.read(&received_data, sizeof(Received_data));
 
     //Checks the value received and acts depending on the value received, if none are valid then it resets the incoming information
     if(received_data.light_status == 1)
-    {
-      digitalWrite(BUTTON_LED_POWER_PIN, received_data.light_status);
-    }
+      digitalWrite(BUTTON_LED_POWER_PIN, received_data.light_status); //1 == HIGH
     else
-    {
       resetData();
-    }
   }
 }
 
-
-/**************************************************/
+/**********************LOOP************************/
 
 void loop()
 {
   //Receive the radio data
   receive_the_data();
-  /*
+ 
   //If light_status is ON, then enable the button to turn it off
   if(received_data.light_status == 1)
   {
@@ -89,8 +81,7 @@ void loop()
     if(BUTTON_STATE == LOW) // button is pressed
     {
       resetData();
-      digitalWrite(BUTTON_LED_POWER_PIN, received_data.light_status);
+      digitalWrite(BUTTON_LED_POWER_PIN, received_data.light_status); //0 == LOW
     }
   }
-  */
 }
